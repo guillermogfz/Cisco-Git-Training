dec21a3827e96328d44dad8a7612abea72d7e40a
guillermogfz <guillermogf95@gmail.com>
2020-02-06 15:29:08 +0100

    Modified if-else

--- if_else_example.py
+++ if_else_example.py
@@ -1,5 +1,17 @@
-x = 'boo'
-if x == 'boo':
-    print ('Eek')
+def main():
+    password=input("Please enter your password::")
+if password=="student":
+    student()
+elif password=="teacher":
+    teacher()
+elif password=="headmaster":
+    headmaster()
 else:
-    print ('Anyone there?')
+    print("incorrect password. We cannot take you any further, goodbye!")
+def student():
+    print("****WELCOME STUDENT****")
+def teacher():
+    print("****WELCOME TEACHER****")
+def headmaster():
+    print("****WELCOME HEADMASTER****")
+main()
